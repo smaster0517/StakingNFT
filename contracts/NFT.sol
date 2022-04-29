1d11090341bccf19b1a22d97e184bdf4c2926390
smaster0517 <smaster0517@gmail.com>
2022-04-29 05:57:56 -0400

    Update NFT.sol

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
 
     constructor (
         string memory name,
         string memory symbol
-    ) ERC721(name, symbol) {
-    
+    ) ERC721(name, symbol) {    
     }
     
     function mint(address _to, uint _tokenId) external {
